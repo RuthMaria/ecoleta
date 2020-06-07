715953fb8293fb1d4b33e558b294136f0f0bc0d0
RuthMaria <ruth-15rms@hotmail.com>
2020-06-07 15:43:45 -0300

    Cadastrei os dados no banco, modifiquei o css do modal de create-point, fiz o tratamento do modal de create-point para aparecer quando o cadastrado é finalizado e quando há erro no banco, criei o modal de erro do create-point

--- src/server.js
+++ src/server.js
@@ -11,6 +11,9 @@ dentro de public, diretamente*/
 
 server.use(express.static('public'))
 
+// habilita o uso do body-parser
+server.use(express.urlencoded({extended:true}))
+
 nunjucks.configure('src/views', {
     express: server,
     noCache: true
@@ -26,9 +29,49 @@ server.get('/create-point', (req, res) => {
     return res.render('create-point.html')
 })
 
+server.post('/savepoint', (req, res) => {
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);`
+    
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData (err) {
+        if (err) {
+            console.log(err)
+            return res.render("create-point.html", {error: true})
+        }
+        return res.render("create-point.html", {saved: true})
+    }
+//afterInsertData está sem parametro, pois não queremos que execute imediatamente, funcione apenas como callback
+    db.run(query, values, afterInsertData)
+})
+
 server.get('/search', (req, res) => {
 
-    db.all(`SELECT * FROM places`, function (err, rows) {
+    const search = req.query.search
+
+    if (search == "") {
+        return res.render('search-results.html', {total:0})
+    }
+
+    db.all(`SELECT * FROM places WHERE city = '${search}'`, function (err, rows) {
         if (err) {
             return console.log(err)
         }
